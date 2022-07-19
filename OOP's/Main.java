c6e09702ccbcdff0daa7438781551304e25eb2ee
Yash Chaudhary <47343850+Yashchaudhary-0@users.noreply.github.com>
2022-07-19 16:15:45 +0000

    implement user define function and call a function/methods.

---
+++ OOP's/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    static void myMethod(){
+        System.out.println("Hey Ayush! Done with your assignment?");
+    }
+
+    public static void main(String[] arrgs){
+        myMethod();
+    }
+}
